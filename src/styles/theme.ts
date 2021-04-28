14f34ec1391e90aad71e9eadbc1f5b6a890d9947
Your Name <you@example.com>
2021-04-28 04:24:17 -0400

    fisrt push

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export default {
+  colors: {
+    background: 'radial-gradient(#e66465, #9198e5)',
+    black: '#282c34',
+    blue: '#a0e9fd',
+    lightBlue: '#caf3fe',
+    white: 'white',
+  },
+  transition: '0.3s',
+}
